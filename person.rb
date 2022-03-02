c418c8e08c936d222dd4bb3387a31061587d4fb0
Khalil <khalilnasri92@gmail.com>
2022-03-02 20:55:37 +0100

    added classroom, book and rental classes

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ require './corrector'
 
 class Person
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(age, name = 'unknown', parent_permission: true)
     @id = Random.rand(1..100)
@@ -10,19 +10,24 @@ class Person
     @age = age
     @parent_permission = parent_permission
     @corrector = Corrector.new
+    @rentals = []
   end
 
-  def can_use_service?
-    of_age? || parent_permission
+  def can_use_service
+    of_age || @parent_permission
   end
 
   def validate_name
     @name = @corrector.correct_name(@name)
   end
 
+  def add_rental(rental)
+    @rentals << rental
+  end
+
   private
 
-  def of_age?
+  def of_age
     @age >= 18
   end
 end
